79d8c120e7605ceadf246b73d3f261c8c207a86a
MERIAM AYER <ayer.meryam@gmail.com>
2024-11-04 10:23:30 +0100

    ajouter le projet

--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { User } from '../../models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +8,6 @@ import { inject, Injectable } from '@angular/core';
 export class LoginService {
 
   private http = inject(HttpClient);
+  user = signal<User | null | undefined>(undefined);
   constructor() { }
 }
